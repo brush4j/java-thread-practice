9bf7445bded2398e551e1b4558302f00c31520e7
Halfmoonly <liuyanoutsee@outlook.com>
2025-05-18 12:52:50 +0800

    init module sync-cas-practice

---
+++ sync-cas-practice/src/main/java/com/lyflexi/synccaspractice/SyncCasApplication.java
@@ -0,0 +1,13 @@
+package com.lyflexi.synccaspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncCasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncCasApplication.class, args);
+    }
+
+}
---
+++ sync-cas-practice/src/test/java/com/lyflexi/synccaspractice/SyncCasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyflexi.synccaspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncCasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
